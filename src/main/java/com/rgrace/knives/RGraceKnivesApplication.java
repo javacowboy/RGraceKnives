7f1d975598bbda1dbdbc2a75329fc48fb38f123f
Matthew Young <matthew.young@ldschurch.org>
2015-04-14 22:24:02 -0600

    Initial checkin

---
+++ src/main/java/com/rgrace/knives/RGraceKnivesApplication.java
@@ -0,0 +1,12 @@
+package com.rgrace.knives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RGraceKnivesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RGraceKnivesApplication.class, args);
+    }
+}
